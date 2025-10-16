457e3ec9da9c5ba320218429bf7654de6dda17b0
Lukas <LukasHoltbrink@web.de>
2025-10-16 16:01:28 +0200

    Fix type issues for RecipesCard

--- app/recipesdetails/[slug]/page.tsx
+++ app/recipesdetails/[slug]/page.tsx
@@ -16,13 +16,28 @@ export default async function Page({ params }: { params: Promise<Params> }) {
     },
   });
 
+    if (!recipe) {
+    return (
+      <div className="flex flex-col items-center mt-10">
+        <h1 className="text-2xl font-semibold">Rezept nicht gefunden</h1>
+      </div>
+    );
+  }
+
+    const recipeForCard = {
+    ...recipe,
+    image: recipe.image || null,
+    duration: recipe.duration.toString(),
+    potion: recipe.potion.toString(),
+  };
+
   return (
     <div className='flex flex-col items-center relative'>
       <div className='bg-[#FF6048] top-0 w-full mb-5 h-[275px] md:h-[50px]'>
         < NavBar showSearch={false} showGenre={false}/>
       </div>
       <div className='w-full absolute top-15'>
-        <RecipesCard recipe={recipe} variant="detail"/>
+         <RecipesCard recipe={recipeForCard} variant="detail"/>
       </div>
     </div>
   )
